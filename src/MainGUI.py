a78970fac51ee0363dace9cdaeb4d9c47ce7a30c
kbntt <kbntt73@gmail.com>
2020-05-02 19:55:46 +0900

    시리얼 통신 테스트 화면 작성

--- src/MainGUI.py
+++ src/MainGUI.py
@@ -1,7 +1,9 @@
 import sys
 from PyQt5 import uic
 from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QStandardItemModel
 from PyQt5.QtWidgets import *
+import src.comm.SerialComm as SerialComm
 
 main_form = uic.loadUiType("../GUI/Main.ui")[0]
 
@@ -11,10 +13,23 @@ class MainGUI(QMainWindow, main_form):
         super().__init__()
 
         self.setupUi(self)
-        self.pushBtn_Start.clicked.connect(lambda: self.StartClicked())
-        self.pushBtn_Stop.clicked.connect(lambda: self.StopClicked())
         self.setWindowIcon(QIcon('../GUI/img/icons-motorcycle-01.png'))
 
+        # MAIN Tab
+        self.pushBtn_Start.clicked.connect(lambda: self.StartClicked())
+        self.pushBtn_Stop.clicked.connect(lambda: self.StopClicked())
+
+        # COMM Tab
+        for i in SerialComm.serial_ports():
+            self.comboBox_comPort.addItem(i)
+        """
+        self.btn_PortOpen.connect(lambda: self.PortOpenClicked())
+        self.btn_PortClose.connect(lambda: self.PortCloseClicked())
+        self.btn_DataSend.connect(lambda: self.DataSendClicked())
+        self.btn_Clear.connect(lambda: self.ClearClicked())
+        """
+
+    # MAIN Tab
     def StartClicked(self):
         print('StartClicked')
         self.lineEdit_CurrTemp.setText('StartClicked')
@@ -23,13 +38,37 @@ class MainGUI(QMainWindow, main_form):
         print('StopClicked')
         self.lineEdit_Humidity.setText('StartClicked')
 
+    # COMM Tab
+
+    def PortOpenClicked(self):
+        print('PortOpenClicked')
+        model = QStandardItemModel(self)
+        model.appendRow("PortOpenClicked")
+        self.listView_Data_Recieve.setModel(model)
+
+    
+    def PortCloseClicked(self):
+        print('PortCloseClicked')
+        model = QStandardItemModel(self)
+        model.appendRow("PortCloseClicked")
+        self.listView_Data_Recieve.setModel(model)
+
+    def DataSendClicked(self):
+        print('DataSendClicked')
+        model = QStandardItemModel(self)
+        model.appendRow("DataSendClicked")
+        self.listView_Data_Recieve.setModel(model)
+
+    def ClearClicked(self):
+        print('ClearClicked')
+        model = QStandardItemModel(self)
+        model.appendRow("ClearClicked")
+        self.listView_Data_Recieve.setModel(model)
+    """
+
 
 if __name__ == '__main__':
     # Serial 통신
-    print("=== serial_ports() ===")
-    # print(serial_ports())
-    print("### serial_ports() ###")
-
     app = QApplication(sys.argv)
     mainWindow = MainGUI()
     mainWindow.show()
--- src/comm/SerialComm.py
+++ src/comm/SerialComm.py
@@ -1,7 +1,30 @@
+import sys
+import glob
+import serial
 
-def add(a, b):
-    return a + b
+
+def serial_ports():
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
 
 
-def mul(a, b):
-    return a * b
+class SerialComm:
+    def __init__(self):
+        pass
