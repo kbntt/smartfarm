a8e528daf8c67d66ba8fc2d690fcd7db19300248
kbntt <kbntt73@gmail.com>
2020-05-08 22:10:20 +0900

    시리얼 통신 테스트 화면 작성

--- src/MainGUI.py
+++ src/MainGUI.py
@@ -4,12 +4,19 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtGui import QStandardItemModel
 from PyQt5.QtWidgets import *
 import src.comm.SerialComm as SerialComm
+import threading
+import time
+import queue  # MK: queue를 사용하기 위해 추가
+import time  # MK: time을 사용하기 위해 추가
+import tkinter  # MK: tkinter(GUI)를 사용하기 위해 추가
 
 main_form = uic.loadUiType("../GUI/Main.ui")[0]
+serialPort = SerialComm.SerialPort()
 
 
 class MainGUI(QMainWindow, main_form):
-    def __init__(self):
+
+    def __init__(self, message=None):
         super().__init__()
 
         self.setupUi(self)
@@ -20,13 +27,21 @@ class MainGUI(QMainWindow, main_form):
         self.pushBtn_Stop.clicked.connect(lambda: self.StopClicked())
 
         # COMM Tab
-        for i in SerialComm.serial_ports():
-            self.comboBox_comPort.addItem(i)
+        for index in SerialComm.serial_ports():
+            self.comboBox_comPort.addItem(index)
 
         self.btn_PortOpen.clicked.connect(lambda: self.PortOpenClicked())
         self.btn_PortClose.clicked.connect(lambda: self.PortCloseClicked())
-        self.btn_DataSend.clicked.connect(lambda: self.DataSendClicked())
+
+        self.btn_Temp_Send.clicked.connect(lambda: self.TempSendClicked())
+        self.btn_Humidity_Send.clicked.connect(lambda: self.HumiditySendClicked())
+        self.btn_Mortor_Move_Send.clicked.connect(lambda: self.MortorMoveSendClicked())
+        self.btn_Mortor_State_Send.clicked.connect(lambda: self.MortorStateSendClicked())
+
         self.btn_Clear.clicked.connect(lambda: self.ClearClicked())
+
+        serialPort.Open('COM3', 9600)
+        serialPort.RegisterReceiveCallback(self.OnReceiveSerialData)
 
     # MAIN Tab
     def StartClicked(self):
@@ -43,22 +58,46 @@ class MainGUI(QMainWindow, main_form):
         print('PortOpenClicked')
         self.listWidget_Data_Recieve.addItem('PortOpenClicked')
 
-
     def PortCloseClicked(self):
         print('PortCloseClicked')
         self.listWidget_Data_Recieve.addItem('PortCloseClicked')
 
-    def DataSendClicked(self):
-        print('DataSendClicked')
-        self.listWidget_Data_Recieve.addItem('DataSendClicked')
+    def TempSendClicked(self):
+        print('TempSendClicked')
+        message = self.textEdit_Temp_Send.toPlainText()
+        serialPort.Send(message)
+        self.listWidget_Data_Recieve.addItem('TempSendClicked')
+
+    def HumiditySendClicked(self):
+        print('HumiditySendClicked')
+        message = 'M0'
+        serialPort.Send(message)
+        self.listWidget_Data_Recieve.addItem('HumiditySendClicked')
+
+    def MortorMoveSendClicked(self):
+        print('MortorMoveSendClicked')
+        message = 'M0'
+        serialPort.Send(message)
+        self.listWidget_Data_Recieve.addItem('MortorMoveSendClicked')
+
+    def MortorStateSendClicked(self):
+        print('MortorStateSendClicked')
+        message = 'M0'
+        serialPort.Send(message)
+        self.listWidget_Data_Recieve.addItem('MortorStateSendClicked')
 
     def ClearClicked(self):
         print('ClearClicked')
         self.listWidget_Data_Recieve.addItem('ClearClicked')
 
+    # serial data callback function
+    def OnReceiveSerialData(self):
+        print("OnReceiveSerialData")
+        print(serialPort.receivedMessage)
+        self.listWidget_Data_Recieve.addItem(serialPort.receivedMessage)
+
 
 if __name__ == '__main__':
-    # Serial 통신
     app = QApplication(sys.argv)
     mainWindow = MainGUI()
     mainWindow.show()
--- src/comm/SerialComm.py
+++ src/comm/SerialComm.py
@@ -1,7 +1,8 @@
-import sys
 import glob
+import sys
 import serial
-
+import _thread
+import time
 
 def serial_ports():
     if sys.platform.startswith('win'):
@@ -25,6 +26,122 @@ def serial_ports():
     return result
 
 
-class SerialComm:
+class SerialPort:
     def __init__(self):
-        pass
+        self.comportName = ""
+        self.baud = 0
+        self.timeout = None
+        self.ReceiveCallback = None
+        self.isopen = False
+        self.receivedMessage = None
+        self.serialport = serial.Serial()
+        #self.serialport = serial.Serial('COM3', 9600)
+
+        print('__init__')
+
+    def __del__(self):
+        try:
+            if self.serialport.is_open():
+                self.serialport.close()
+        except:
+            print("Destructor error closing COM port: ", sys.exc_info()[0])
+
+    def RegisterReceiveCallback(self, aReceiveCallback):
+        print('RegisterReceiveCallback')
+        self.ReceiveCallback = aReceiveCallback
+        try:
+            _thread.start_new_thread(self.SerialReadlineThread, ())
+        except:
+            print("Error starting Read thread: ", sys.exc_info()[0])
+
+    def SerialReadlineThread(self):
+        line = []
+        print('SerialReadlineThread')
+        while True:
+            #print("receivedMessage 1")
+            try:
+                #print("receivedMessage 2")
+                if self.isopen:
+
+                    # 데이터가 있있다면
+                    for c in self.serialport.read():
+                        # line 변수에 차곡차곡 추가하여 넣는다.
+                        #print("chr(c)", chr(c))
+                        line.append(str(chr(c)))
+
+                        if str(chr(c)) == '\r':  # 라인의 끝을 만나면
+                            # 데이터 처리 함수로 호출
+                            print("22222")
+                            # print(line)
+                            print(''.join(line))
+                            self.receivedMessage = ''.join(line)
+                            print("self.receivedMessage", self.receivedMessage)
+                            if self.receivedMessage != "":
+                                self.ReceiveCallback()
+                            # line 변수 초기화
+                            del line[:]
+                            self.receivedMessage = None
+                            # print("44444")
+
+                    """
+                    self.receivedMessage = self.serialport.readline()
+                    print("receivedMessage")
+                    print(self.serialport.readline())
+                    if self.receivedMessage != "":
+                        self.ReceiveCallback(self.receivedMessage)
+                        """
+            except:
+                print("Error reading COM port: ", sys.exc_info()[0])
+
+    def IsOpen(self):
+        return self.isopen
+
+    def Open(self, portname, baudrate):
+        if not self.isopen:
+            # serialPort = 'portname', baudrate, bytesize = 8, parity = 'N',
+            # stopbits = 1, timeout = None, xonxoff = 0, rtscts = 0)
+            self.serialport.port = portname
+            self.serialport.baudrate = baudrate
+            try:
+                self.serialport.open()
+                self.isopen = True
+            except:
+                print("Error opening COM port: ", sys.exc_info()[0])
+
+    def Close(self):
+        if self.isopen:
+            try:
+                self.serialport.close()
+                self.isopen = False
+            except:
+                print("Close error closing COM port: ", sys.exc_info()[0])
+
+    def Send(self, message):
+        print('Send', self.isopen)
+        print('message', message)
+        if self.isopen:
+            try:
+                # Ensure that the end of the message has both \r and \n, not just one or the other
+
+                newmessage = message.strip()
+                newmessage += '\r\n'
+                self.serialport.write(newmessage.encode('utf-8'))
+                """
+                print("Send:", str.encode('M0'))
+                self.serialport.write(str.encode('M0'))
+                """
+            except:
+                print("Error sending message: ", sys.exc_info()[0])
+            else:
+                return True
+        else:
+            return False
+
+    # 데이터 처리할 함수
+    def parsing_data(data):
+        # 리스트 구조로 들어 왔기 때문에
+        # 작업하기 편하게 스트링으로 합침
+        print(data)
+        print(type(data))
+        tmp = ''.join(data)
+        print(tmp)
