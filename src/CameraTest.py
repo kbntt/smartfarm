1a45dfe086c38c78d63f576b527279fe00c4a826
kbntt <kbntt73@gmail.com>
2020-05-21 16:26:06 +0900

    카메라 작업

--- src/CameraTest.py
+++ src/CameraTest.py
@@ -1,3 +1,20 @@
-import cv2
+import cv2 as cv
 
-print(cv2.__version__)
+cap = cv.VideoCapture(0)
+
+while True:
+    ret, img_color = cap.read()
+
+    if not ret:
+        break
+
+    img_gray = cv.cvtColor(img_color, cv.COLOR_BGR2GRAY)
+
+    cv.imshow('bgr', img_color)
+    cv.imshow('gray', img_gray)
+
+    if cv.waitKey(1) & 0xFF == 27:
+        break
+
+cap.release()
+cv.destroyAllWindows()
