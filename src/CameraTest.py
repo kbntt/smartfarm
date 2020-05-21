4ba14b07d8d8c688ca454c801995b1a27072bb6f
kbntt <kbntt73@gmail.com>
2020-05-21 16:28:02 +0900

    카메라 작업

--- src/CameraTest.py
+++ src/CameraTest.py
@@ -8,10 +8,7 @@ while True:
     if not ret:
         break
 
-    img_gray = cv.cvtColor(img_color, cv.COLOR_BGR2GRAY)
-
     cv.imshow('bgr', img_color)
-    cv.imshow('gray', img_gray)
 
     if cv.waitKey(1) & 0xFF == 27:
         break
